bdc1be9f16a88497165c44e46191443c401340e7
josuebarretogit <josuebarretogit@gmail.com>
2024-06-05 22:30:01 -0500

    implemented method replace for vec<t>

--- src/vec.rs
+++ src/vec.rs
@@ -1,5 +1,71 @@
 pub mod methods;
 
+#[cfg(test)]
+mod tests {
 
+    use crate::vec::methods::*;
 
+    #[derive(Debug, Default, Clone, PartialEq)]
+    struct Example {
+        name: String,
+        id: i32,
+        other_property: Vec<u8>,
+    }
 
+    #[test]
+    fn replace_method_works() {
+        let case1 = vec![1, 3, 4, 5];
+
+        let replaced_vec = case1.replace(|item| *item == 3, 4);
+
+        assert_eq!(vec![1, 4, 4, 5], replaced_vec);
+
+        let replaced_vec2 = case1.replace(|item| *item == 5, 10);
+
+        assert_eq!(vec![1, 3, 4, 10], replaced_vec2);
+
+        let case2: Vec<Example> = vec![
+            Example {
+                name: "josh".to_string(),
+                id: 1,
+                other_property: vec![],
+            },
+            Example {
+                name: "ua".to_string(),
+                id: 2,
+                other_property: vec![],
+            },
+        ];
+
+        let replaced_struct_vec = case2.replace(
+            |example| example.name.as_str() == "josh",
+            Example {
+                name: "replaced".to_string(),
+                id: 3,
+                other_property: vec![1, 2],
+            },
+        );
+
+        assert_eq!(vec![
+            Example {
+                name: "replaced".to_string(),
+                id: 3,
+                other_property: vec![1, 2],
+            },
+            Example {
+                name: "ua".to_string(),
+                id: 2,
+                other_property: vec![],
+            },
+        ], replaced_struct_vec);
+    }
+
+    #[test]
+    fn replace_mut_method_works() {
+        let mut case1 = vec![1, 2, 3, 4, 5];
+
+        case1.replace_mut(|item| *item == 2, 3);
+
+        assert_eq!(vec![1, 3, 3, 4, 5], case1);
+    }
+}
--- src/vec/methods.rs
+++ src/vec/methods.rs
@@ -1,22 +1,39 @@
 pub trait ReplaceVec<F, V>
 where
-    F: FnOnce() -> bool,
-    V: PartialEq,
+    F: FnMut(&V) -> bool,
+    V: PartialEq + Clone,
 {
-    fn replace(&self, searcher: F, value: V) -> &Self;
+    fn replace(&self, searcher: F, value: V) -> Vec<V>;
     fn replace_mut(&mut self, searcher: F, value: V);
 }
 
 impl<F, V> ReplaceVec<F, V> for Vec<V>
 where
-    F: FnOnce() -> bool,
-    V: PartialEq,
+    F: FnMut(&V) -> bool,
+    V: PartialEq + Clone,
 {
-    fn replace(&self, searcher: F, value: V) -> &Self {
-        todo!()
+    fn replace(&self, searcher: F, value: V) -> Vec<V> {
+        let item = self.iter().position(searcher);
+        match item {
+            Some(index) => {
+                let mut new_vec: Vec<V> = vec![];
+                for item in self {
+                    if *item == self[index] {
+                        new_vec.push(value.clone());
+                    } else {
+                        new_vec.push(item.clone());
+                    }
+                }
+                new_vec
+            }
+            None => self.clone(),
+        }
     }
 
     fn replace_mut(&mut self, searcher: F, value: V) {
-        todo!()
+        let item_to_replace_index = self.iter().position(searcher);
+        if let Some(index) = item_to_replace_index {
+            self[index] = value;
+        }
     }
 }
