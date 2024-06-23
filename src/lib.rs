16c3b2086d27821f6916e307c4e7d17d72fc10ef
josuebarretogit <josuebarretogit@gmail.com>
2024-06-23 15:28:46 -0500

    updated readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
 #![warn(clippy::pedantic)]
 
+#[cfg(feature = "strings")]
 pub mod strings;
  
+#[cfg(feature = "vec")]
 pub mod vec;
 
+#[cfg(feature = "bool")]
 pub mod bool;
 
+#[cfg(feature = "option")]
 pub mod option;
 
+#[cfg(feature = "result")]
 pub mod result;
